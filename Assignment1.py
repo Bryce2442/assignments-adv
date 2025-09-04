d01997eb157ae80e9963e22824ac18a3d480a1d1
Bryce2442 <barmand@students.kennesaw.edu>
2025-09-04 16:47:51 -0400

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,37 @@
+# Program Name: Assignment1.py
+# Course: IT3883/Section 1
+# Student Name: Bryce Armand
+# Assignment Number: Lab#
+# Due Date: 9/5/ 2025
+# Purpose: To make a menu that will loop and continusly add or clear buffers until it is turned off by a user input.
+# List Specific resources used to complete the assignment. while loop, if, else.
+
+choice = ""
+
+# Ask for choice input at the start
+buffer = input("Enter initial buffer data: ")
+
+while choice != "4":
+    #loops the program until 4 is chosen
+    print("\nMenu:")
+    print("1. Append data to the input buffer")
+    print("2. Clear the input buffer")
+    print("3. Display the input buffer")
+    print("4. Exit the program")
+
+    choice = input("Enter your choice (1-4): ")
+#entering choice
+    if choice == "1":
+        data = input("Enter a string to append: ")
+        buffer += data   # appends then prints that the data was appended
+        print("Data appended.")
+    elif choice == "2":
+        buffer = ""  #if 2 is selected buffer gets cleared
+        print("Buffer cleared.")
+    elif choice == "3":
+        print("Current buffer:", buffer)  # prints the current buffer
+    elif choice != "4":
+        print("Invalid choice. Please enter 1-4.") # end of the loop
+
+print("Exiting program.")
+
